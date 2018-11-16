80192f6f80e473cd0cda6056d602486cd263f080
JeffCh4ng <jeffry.chang70@gmail.com>
2018-11-16 11:33:17 +0700

    merge jefry

---
+++ Binus/Binus/Models/AssessmentSensory/ScoreSensory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Binus.Models.AssessmentSensory
+{
+    public class ScoreSensory
+    {
+    }
+}
---
+++ Binus/Binus/Models/ScoreIntelligence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Binus.Models
+{
+    public class ScoreIntelligence
+    {
+        public int scoreValue { get; set; }
+        public string scoreWord { get; set; }
+    }
+}
