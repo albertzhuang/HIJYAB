c9fd481035a1a12db80eb8a2f6ffc36290541e0e
zhuangalbert <albertzuang@gmail.com>
2018-11-21 18:19:31 +0700

    update view on sensory assessment

--- Binus/Controllers/Api/AssessmentTypeController.cs
+++ Binus/Controllers/Api/AssessmentTypeController.cs
@@ -18,14 +18,11 @@ namespace Binus.Controllers.Api
         [HttpGet]
         public HttpResponseMessage getAll()
         {
-            //return new HttpResponseMessage(HttpStatusCode.OK);
             try
             {
                 var result = new HttpResponseMessage(HttpStatusCode.OK);
-                result.Content = new StringContent(JsonConvert.SerializeObject(db.AssessmentTypes1.ToList(), new JsonSerializerSettings()
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                }));
+                result.Content = new StringContent(JsonConvert.SerializeObject(db.AssessmentTypes1.ToList()));
+
                 result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                 return result;
             }
@@ -33,19 +30,6 @@ namespace Binus.Controllers.Api
             {
                 return new HttpResponseMessage(HttpStatusCode.BadRequest);
             }
-            //try
-            //{
-            //    var result = new HttpResponseMessage(HttpStatusCode.OK);
-            //    //result.Content = new StringContent(JsonConvert.SerializeObject(db.AssessmentTypes1.ToList()));
-                
-            //    result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            //    return result;
-            //}
-            //catch
-            //{
-            //    return new HttpResponseMessage(HttpStatusCode.BadRequest);
-            //}
         }
     }
 }
