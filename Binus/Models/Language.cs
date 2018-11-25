248948d845f92cc270ed4069d65a111a0f13ee94
zhuangalbert <albertzuang@gmail.com>
2018-11-26 01:12:13 +0700

    insert language

---
+++ Binus/Models/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Binus.Models
+{
+    public class Language
+    {
+        public int languageID { get; set; }
+        public string language { get; set; }
+    }
+}
